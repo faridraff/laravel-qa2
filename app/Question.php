a801eb1dbc28b89ea38fa07869afe62d31321932
Farid Rofiq <test@youremail.com>
2020-05-09 16:00:42 +0700

    propulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -10,4 +10,11 @@ class Question extends Model
     public function user(){
         return $this->belongsTo(user::class);
     }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title']= $value;
+        $this->attributes['slug']= str_slug($value);
+        
+    }
 }
