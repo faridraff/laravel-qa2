754ed0672ae2cfe47e21074a4e60783fb60f458f
Farid Rofiq <test@youremail.com>
2020-05-21 17:31:27 +0700

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -31,5 +31,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute()
+    {
+        return '#';
+    }
     
 }
